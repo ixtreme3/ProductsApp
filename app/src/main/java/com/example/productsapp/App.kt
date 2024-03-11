e477c9ec0159895c09b1ff801d96ab07d68bd034
Ivan <ixtreme3@yandex.ru>
2024-03-11 14:06:22 +0700

    Added DynamicColors and other minor layout improvements

--- app/src/main/java/com/example/productsapp/App.kt
+++ app/src/main/java/com/example/productsapp/App.kt
@@ -1,7 +1,14 @@
 package com.example.productsapp
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
+import com.google.android.material.color.utilities.DynamicColor
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
--- app/src/main/java/com/example/productsapp/MainActivity.kt
+++ app/src/main/java/com/example/productsapp/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.productsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolBar))
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val config = AppBarConfiguration(navController.graph)
+        findViewById<MaterialToolbar>(R.id.toolBar).setupWithNavController(navController, config)
     }
 }
